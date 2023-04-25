d458430e56419511b6b35bb51093b501757b3929
myxans <mushenko1992@gmail.com>
2023-04-25 21:41:58 +0300

    add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const TodoList = () => {
+  const [todos, setTodos] = useState([]); // стан для збереження списку тудушок
+  const [newTodo, setNewTodo] = useState(""); // стан для збереження нової тудушки
+
+  // функція для додавання нової тудушки
+  const addTodo = () => {
+    if (newTodo.trim() !== "") {
+      setTodos([...todos, { task: newTodo, completed: false }]);
+      setNewTodo(""); // очистити значення інпуту
+    }
+  };
+
+  // функція для переключення стану виконаності тудушки
+  const toggleTodo = (index) => {
+    const updatedTodos = [...todos];
+    updatedTodos[index].completed = !updatedTodos[index].completed;
+    setTodos(updatedTodos);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Список тудушок</h1>
+      <ul>
+        {todos.map((todo, index) => (
+          <li
+            key={index}
+            onClick={() => toggleTodo(index)}
+            style={{
+              textDecoration: todo.completed ? "line-through" : "none", // встановлюємо стиль для виконаних тудушок
+              cursor: "pointer", // змінюємо курсор на покажчик при наведенні на тудушку
+            }}
+          >
+            {todo.task}
+          </li>
+        ))}
+      </ul>
+      <div>
+        <input
+          type="text"
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+          placeholder="Введіть нову тудушку"
+        />
+        <button onClick={addTodo}>Додати</button>
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default TodoList;
